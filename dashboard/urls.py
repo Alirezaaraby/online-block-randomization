48100ab50e1b509debd4194f33bc45bf5355da36
alirezaaraby <alirezaaraby5@gmail.com>
2023-08-15 04:58:51 -0700

    csv output added

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path("", views.index, name="view.patient"),
     path("edit/<int:id>", views.edit, name="edit.patient"),
+    path("export_to_csv/", views.export_to_csv, name="csv.patient"),
 ]
--- dashboard/views.py
+++ dashboard/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, redirect
 from home.models import patient, state
 from home.forms import PatientForm
+from django.http import HttpResponse
+import csv
 
 
 def index(request):
@@ -20,3 +22,19 @@ def edit(request, id):
         return redirect("view.patient")
 
     return render(request, "edit.html", {"data": data, "form": form})
+
+
+def export_to_csv(request):
+    patients = patient.objects.all()
+    response = HttpResponse(content_type="text/csv")
+    response["Content-Disposition"] = 'attachment; filename="file.csv"'
+    writer = csv.writer(response)
+    writer.writerow(
+        ["name", "family name", "sex", "age", "code", "group", "registery time"]
+    )
+    patient_fields = patients.values_list(
+        "name", "f_name", "sex", "age", "code", "group", "created_at"
+    )
+    for p in patients:
+        writer.writerow([p.name, p.f_name, p.sex, p.age, p.code, p.group, p.created_at])
+    return response
