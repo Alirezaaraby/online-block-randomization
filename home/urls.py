c446a4476fd32a127e0dd433c68053fa9990ab7f
alirezaaraby <alirezaaraby5@gmail.com>
2023-08-15 08:19:36 -0700

    user system added

--- home/urls.py
+++ home/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.index),
+    path("", views.index, name="home"),
     path("patient/", views.patient_handler, name="create.patient"),
 ]
